043205557d9b22d1c6ded59720a1b2d61a59d652
Richard <rick_pendlebury@hotmail.com>
2016-08-28 13:03:13 +0100

    Reformat app main activity

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -16,7 +16,6 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
-import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
@@ -36,66 +35,26 @@ import com.example.android.sunshine.app.gcm.RegistrationIntentService;
 import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.wearable.DataApi;
-import com.google.android.gms.wearable.DataMap;
-import com.google.android.gms.wearable.PutDataMapRequest;
-import com.google.android.gms.wearable.PutDataRequest;
-import com.google.android.gms.wearable.Wearable;
-import com.google.android.gms.wearable.WearableListenerService;
 
-import java.util.Date;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+public class MainActivity extends AppCompatActivity implements ForecastFragment.Callback {
 
-public class MainActivity extends AppCompatActivity implements ForecastFragment.Callback,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
-
-    private final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
     private static final String DETAILFRAGMENT_TAG = "DFTAG";
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
-
-    private static final String TAG = "MainActivity________";
-    private static final String COUNT_PATH = "/count";
-    private static final String COUNT_KEY = "count";
-    private static final String IMAGE_PATH = "/image";
-    private static final String IMAGE_KEY = "photo";
-
-    //Request code for launching the Intent to resolve Google Play services errors.
-    private static final int REQUEST_RESOLVE_ERROR = 1000;
-
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     private boolean mTwoPane;
     private String mLocation;
 
-    private GoogleApiClient mGoogleApiClient;
-    private boolean mResolvingError = false;
-
-    private ScheduledExecutorService mGeneratorExecutor;
-    private ScheduledFuture<?> mDataItemGeneratorFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Build a new GoogleApiClient
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-        //mGeneratorExecutor = new ScheduledThreadPoolExecutor(1);
-
-
         mLocation = Utility.getPreferredLocation(this);
         Uri contentUri = getIntent() != null ? getIntent().getData() : null;
 
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
@@ -123,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             getSupportActionBar().setElevation(0f);
         }
 
-        ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
+        ForecastFragment forecastFragment = ((ForecastFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
         if (contentUri != null) {
@@ -147,88 +106,6 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             if (!sentToken) {
                 Intent intent = new Intent(this, RegistrationIntentService.class);
                 startService(intent);
-            }
-        }
-    }
-
-    // Connect to the data layer when the Activity starts
-    @Override
-    protected void onStart() {
-        super.onStart();
-        //mGoogleApiClient.connect();
-        if (!mResolvingError) {
-            mGoogleApiClient.connect();
-        }
-    }
-
-    // Send a data object when the data layer connection is successful
-    @Override
-    public void onConnected(Bundle connectionHint) {
-
-        ///////////////////////////////////////////
-        // TAKE THIS OUT WHILE RUNNING THE GENERATOR
-        //////////////////////////////////////////
-//        String WEARABLE_DATA_PATH = "/wearable_data";
-//        // Create a DataMap object and send it to the data layer
-//        DataMap dataMap = new DataMap();
-//        long currentTimeInMillis = System.currentTimeMillis();
-//        dataMap.putLong("time", currentTimeInMillis);
-//        //Requires a new thread to avoid blocking the UI
-//        new SendToDataLayerThread(WEARABLE_DATA_PATH, dataMap).start();
-
-        /////////////////////////////////////////////
-        // ONLY UP TO HERE
-        /////////////////////////////////////////////
-
-//        PutDataRequest request = dataMap.putLong("time", currentTimeInMillis);
-//        PutDataRequest request = dataMap.asPutDataRequest();
-//        request.setUrgent();
-//        Wearable.DataApi.putDataItem(mGoogleApiClient, request)
-//                .setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
-//                    @Override
-//                    public void onResult(DataApi.DataItemResult dataItemResult) {
-//                        LOGD(TAG, "Sending data was successful: " + dataItemResult.getStatus()
-//                                .isSuccess());
-//                    }
-//                });
-    }
-
-    // Disconnect from the data layer when the Activity stops
-    @Override
-    protected void onStop() {
-        if (null != mGoogleApiClient && mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
-        }
-        super.onStop();
-    }
-
-    // Placeholders for required connection callbacks
-    @Override
-    public void onConnectionSuspended(int cause) {
-        LOGD(TAG, "Connection to Google API client was suspended");
-//        mStartActivityBtn.setEnabled(false);
-//        mSendPhotoBtn.setEnabled(false);
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        if (!mResolvingError) {
-            if (result.hasResolution()) {
-                try {
-                    mResolvingError = true;
-                    result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
-                } catch (IntentSender.SendIntentException e) {
-                    // There was an error with the resolution intent. Try again.
-                    mGoogleApiClient.connect();
-                }
-            } else {
-                Log.e(TAG, "Connection to Google API client has failed");
-                mResolvingError = false;
-//                mStartActivityBtn.setEnabled(false);
-//                mSendPhotoBtn.setEnabled(false);
-//                Wearable.DataApi.removeListener(mGoogleApiClient, this);
-//                Wearable.MessageApi.removeListener(mGoogleApiClient, this);
-//                Wearable.CapabilityApi.removeListener(mGoogleApiClient, this);
             }
         }
     }
@@ -260,20 +137,16 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
     @Override
     protected void onResume() {
         super.onResume();
-//        mGoogleApiClient.connect();
-//        mDataItemGeneratorFuture = mGeneratorExecutor.scheduleWithFixedDelay(
-//                new DataItemGenerator(), 1, 5, TimeUnit.SECONDS);
 
-
-        String location = Utility.getPreferredLocation( this );
+        String location = Utility.getPreferredLocation(this);
         // update the location in our second pane using the fragment manager
-            if (location != null && !location.equals(mLocation)) {
-            ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_forecast);
-            if ( null != ff ) {
+        if (location != null && !location.equals(mLocation)) {
+            ForecastFragment ff = (ForecastFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_forecast);
+            if (null != ff) {
                 ff.onLocationChanged();
             }
-            DetailFragment df = (DetailFragment)getSupportFragmentManager().findFragmentByTag(DETAILFRAGMENT_TAG);
-            if ( null != df ) {
+            DetailFragment df = (DetailFragment) getSupportFragmentManager().findFragmentByTag(DETAILFRAGMENT_TAG);
+            if (null != df) {
                 df.onLocationChanged(location);
             }
             mLocation = location;
@@ -325,74 +198,6 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             return false;
         }
         return true;
-    }
-
-    class SendToDataLayerThread extends Thread {
-        String path;
-        DataMap dataMap;
-
-        // Constructor for sending data objects to the data layer
-        SendToDataLayerThread(String p, DataMap data) {
-            path = p;
-            dataMap = data;
-        }
-
-        public void run() {
-            // Construct a DataRequest and send over the data layer
-            PutDataMapRequest putDMR = PutDataMapRequest.create(path);
-            putDMR.getDataMap().putAll(dataMap);
-            PutDataRequest request = putDMR.asPutDataRequest();
-            DataApi.DataItemResult result = Wearable.DataApi.putDataItem(mGoogleApiClient, request).await();
-            if (result.getStatus().isSuccess()) {
-                Log.v("myTag", "DataMap: " + dataMap + " sent successfully to data layer ");
-            }
-            else {
-                // Log an error
-                Log.v("myTag", "ERROR: failed to send DataMap to data layer");
-            }
-        }
-    }
-
-
-    /**
-     * Generates a DataItem based on an incrementing count.
-     */
-    private class DataItemGenerator implements Runnable {
-
-        private int count = 0;
-
-        @Override
-        public void run() {
-            PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(COUNT_PATH);
-            putDataMapRequest.getDataMap().putInt(COUNT_KEY, count++);
-
-            PutDataRequest request = putDataMapRequest.asPutDataRequest();
-            request.setUrgent();
-
-            LOGD(TAG, "Generating DataItem: " + request);
-            if (!mGoogleApiClient.isConnected()) {
-                return;
-            }
-            Wearable.DataApi.putDataItem(mGoogleApiClient, request)
-                    .setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
-                        @Override
-                        public void onResult(DataApi.DataItemResult dataItemResult) {
-                            if (!dataItemResult.getStatus().isSuccess()) {
-                                Log.e(TAG, "ERROR: failed to putDataItem, status code: "
-                                        + dataItemResult.getStatus().getStatusCode());
-                            }
-                        }
-                    });
-        }
-    }
-
-    /**
-     * As simple wrapper around Log.d
-     */
-    private static void LOGD(final String tag, String message) {
-        if (Log.isLoggable(tag, Log.DEBUG)) {
-            Log.d(tag, message);
-        }
     }
 
 }
